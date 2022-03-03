af405bfed439b2a9cecf4880edb32f5f2d90c248
jaguars.dev <jaguarsdev.com@gmail.com>
2022-03-03 20:41:41 +0330

    problem on font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {
+      fontFamily:{
+        Pop: ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
