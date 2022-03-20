4defb316d71f729fb397dd84143f725bc054d130
jaguarsdev <jaguarsdev.com@gmail.com>
2022-03-20 17:10:26 +0330

    sectionThree V1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    container: {
-      center: true,
-      padding: '1rem',
-    },
+  
     extend: {
-      fontFamily:{
-        Pop: ['Poppins', 'sans-serif']
-      }
+      container: {
+        center: true,
+      },
     },
   },
   plugins: [],
